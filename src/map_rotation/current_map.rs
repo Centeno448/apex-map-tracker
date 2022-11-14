0c22a656bce1d3f82b77cd35262d13406c87110e
Centeno448 <diegocenteno448@gmail.com>
2022-11-13 21:03:39 -0500

    remove unused props

--- src/map_rotation/current_map.rs
+++ src/map_rotation/current_map.rs
@@ -3,14 +3,8 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct CurrentMap {
-    #[serde(rename = "readableDate_start")]
-    pub readable_date_start: String,
-    #[serde(rename = "readableDate_end")]
-    pub readable_date_end: String,
     pub map: String,
     pub code: MapRotationCode,
-    #[serde(rename = "remainingSecs")]
-    pub remaining_secs: u16,
     #[serde(rename = "remainingMins")]
     pub remaining_mins: u8,
     #[serde(rename = "remainingTimer")]
--- src/map_rotation/next_map.rs
+++ src/map_rotation/next_map.rs
@@ -3,14 +3,7 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct NextMap {
-    #[serde(rename = "readableDate_start")]
-    readable_date_start: String,
-    #[serde(rename = "readableDate_end")]
-    readable_date_end: String,
-    map: String,
     pub code: MapRotationCode,
-    #[serde(rename = "DurationInSecs")]
-    duration_in_secs: u16,
     #[serde(rename = "DurationInMinutes")]
     pub duration_in_minutes: u8,
 }
