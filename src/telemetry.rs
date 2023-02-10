794bfebd4651d009aa56a4d94ecc8e1224fc144e
Centeno448 <diegocenteno448@gmail.com>
2023-02-09 19:20:34 -0500

    allow logs from crate only

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,28 +1,31 @@
-use simplelog::{Config, LevelFilter, SimpleLogger, WriteLogger};
+use simplelog::{ConfigBuilder, LevelFilter, SimpleLogger, WriteLogger};
 use std::fs::File;
 
 use crate::configuration::{Environment, Settings};
 
-pub fn init_logger(configuration: &Settings) {
-    let environment = &configuration.environment;
+pub fn init_logger(app_config: &Settings) {
+    let environment = &app_config.environment;
+
+    let log_config = ConfigBuilder::new()
+        .add_filter_allow("apex_map_tracker".into())
+        .build();
 
     match environment {
-        Environment::Local => init_stdout_logger(),
-        Environment::Production => init_file_logger(configuration),
+        Environment::Local => init_stdout_logger(log_config),
+        Environment::Production => init_file_logger(log_config, app_config),
     };
 }
 
-fn init_stdout_logger() {
-    SimpleLogger::init(LevelFilter::Info, Config::default())
-        .expect("Failed to initialize stdout logger");
+fn init_stdout_logger(log_config: simplelog::Config) {
+    SimpleLogger::init(LevelFilter::Info, log_config).expect("Failed to initialize stdout logger");
 }
 
-fn init_file_logger(configuration: &Settings) {
-    let base_path = &configuration.application.base_log_path;
+fn init_file_logger(log_config: simplelog::Config, app_config: &Settings) {
+    let base_path = &app_config.application.base_log_path;
 
     WriteLogger::init(
         LevelFilter::Info,
-        Config::default(),
+        log_config,
         File::create(format!("{}/apex-map-tracker.log", base_path)).unwrap(),
     )
     .expect("Failed to initialize file logger");
